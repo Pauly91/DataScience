840e2015a4496d1550faf1a9ad07797972a66c88
Pauly91 <abelpaul91@gmail.com>
2017-07-06 00:20:56 +0530

    categorical data analysis

--- reason.py
+++ reason.py
@@ -13,33 +13,12 @@ import csv
 import matplotlib
 import matplotlib.pyplot as plt
 from sklearn.cross_validation import StratifiedKFold
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 matplotlib.style.use('ggplot')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+catVar = ['Product_Info_1','Product_Info_2','Product_Info_3','Product_Info_5','Product_Info_6', 'Product_Info_7', 'Employment_Info_2', 'Employment_Info_3', 'Employment_Info_5', 'InsuredInfo_1', 'InsuredInfo_2', 'InsuredInfo_3', 'InsuredInfo_4', 'InsuredInfo_5', 'InsuredInfo_6', 'InsuredInfo_7', 'Insurance_History_1', 'Insurance_History_2', 'Insurance_History_3', 'Insurance_History_4', 'Insurance_History_7', 'Insurance_History_8', 'Insurance_History_9', 'Family_Hist_1', 'Medical_History_2', 'Medical_History_3', 'Medical_History_4', 'Medical_History_5', 'Medical_History_6', 'Medical_History_7', 'Medical_History_8', 'Medical_History_9', 'Medical_History_11', 'Medical_History_12', 'Medical_History_13', 'Medical_History_14', 'Medical_History_16','Medical_History_17', 'Medical_History_18', 'Medical_History_19', 'Medical_History_20', 'Medical_History_21', 'Medical_History_22', 'Medical_History_23', 'Medical_History_25', 'Medical_History_26', 'Medical_History_27', 'Medical_History_28', 'Medical_History_29', 'Medical_History_30', 'Medical_History_31', 'Medical_History_33', 'Medical_History_34', 'Medical_History_35', 'Medical_History_36', 'Medical_History_37', 'Medical_History_38', 'Medical_History_39', 'Medical_History_40', 'Medical_History_41']
 
 
 
@@ -77,6 +56,17 @@ def dataSplitter(df,y,type):
 
 
 
+def categoricalDataAnalysis(df):
+    dtype_df = df[catVar].dtypes.reset_index()
+    dtype_df.columns = ["Count", "Column Type"]
+
+    print(df['Product_Info_2'].value_counts()) # The only categorical data without numerical val
+
+
+
+def categoricalDataHandling(df):
+
+    labelEncoder = LabelEncoder()
 
 
 
@@ -95,8 +85,6 @@ def main():
     dfTrain.drop('Response',axis=1,inplace=True)
     dfTrain.drop('Id', axis=1,inplace=True)
 
-    yCount = y['Response'].value_counts()
-    print(yCount)
     '''
     yCount = y['Response'].value_counts()
     print(yCount)
@@ -107,14 +95,9 @@ def main():
 
     '''
     [x_train, y_train, x_valid, y_valid] = dataSplitter(dfTrain, y, 1)
-
-
-
-    yCount = y_train['Response'].value_counts()
-    print(yCount)
-    yCount = y_valid['Response'].value_counts()
-    print(yCount)
-
+    print(x_train[catVar].head(10))
+    categoricalDataAnalysis(x_train)
+    #categoricalDataHandling(y_train)
 
 
 

6c8c3e576dc3111645e7387fcfecabed90b69e0d
Pauly91 <abelpaul91@gmail.com>
2017-07-06 17:30:53 +0530

    Did PCA on the on categorical data.

--- reason.py
+++ reason.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.decomposition import PCA
 matplotlib.style.use('ggplot')
 
 
@@ -66,7 +67,42 @@ def categoricalDataAnalysis(df):
 
 def categoricalDataHandling(df):
 
+    # Converts labels to numbers i.e encodes the data
     labelEncoder = LabelEncoder()
+    labelEncoder.fit(df['Product_Info_2'])
+    product_Info_2LabelEncoder = labelEncoder.transform(df['Product_Info_2'])
+    df['product_Info_2LabelEncoded'] = product_Info_2LabelEncoder
+
+    df.drop('Product_Info_2', axis = 1, inplace = True)
+    print(df.head(10))
+
+
+    # Refer this : http://biggyani.blogspot.in/2014/08/using-onehot-with-categorical.html
+    # The idea to transform DataFrame to np.array and process it. Read about it more to get a feel of it
+
+
+    train_categorical_values = np.array(df)
+    ohe = OneHotEncoder()
+
+    train_cat_data = ohe.fit_transform(train_categorical_values)
+
+    train_cat_data_df = DataFrame(train_cat_data.toarray())
+    print(train_cat_data_df.describe)
+
+
+
+    # PCA on the data
+
+    pca = PCA(n_components=12) # how to choose the components
+
+    train_cat_data_array = np.array(train_cat_data_df)
+    pca.fit(train_cat_data_df)
+    train_cat_data_PCA = pca.transform(train_cat_data_array)
+    print(len(train_cat_data_PCA))
+    train_cat_data_df_pca = DataFrame(train_cat_data_PCA)
+    print(train_cat_data_df_pca.describe)
+
+
 
 
 
@@ -96,8 +132,8 @@ def main():
     '''
     [x_train, y_train, x_valid, y_valid] = dataSplitter(dfTrain, y, 1)
     print(x_train[catVar].head(10))
-    categoricalDataAnalysis(x_train)
-    #categoricalDataHandling(y_train)
+    categoricalDataAnalysis(x_train[catVar])
+    categoricalDataHandling(x_train[catVar])
 
 
 
